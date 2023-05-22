a86fa13eb802707f03a3bc2e95a7e09d1071b6a4
tbohn2 <tanner.bohn@yahoo.com>
2023-05-21 20:41:08 -0700

    Finished POST route to create new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,8 +34,15 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body, {
+    });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
