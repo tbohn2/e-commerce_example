40f99d16c96d1fd3c5f7a177fac1c2a0819b7353
tbohn2 <tanner.bohn@yahoo.com>
2023-05-21 21:01:47 -0700

    Finished DELETE route to destroy products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,8 +114,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deletedProduct = await Product.destroy(
+      {
+        where: {
+          id: req.params.id,
+        },
+      })
+    res.status(200).json(deletedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
