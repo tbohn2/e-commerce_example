64d7155be5a3099402fe7ea22bb957f655e36254
tbohn2 <tanner.bohn@yahoo.com>
2023-05-21 20:58:23 -0700

    Finished DELETE route to destroy categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -64,8 +64,19 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deletedCategory = await Category.destroy(
+      {
+        where: {
+          id: req.params.id,
+        },
+      })
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
